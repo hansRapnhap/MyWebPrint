23892e78ff221ef8f2ab11cf9f5c26a911b1fd59
Hans Cardoen <hans.cardoen@iptor.com>
2020-11-22 18:29:03 +0100

    awwded web print example

--- app/src/main/java/be/rapnhap/mywebprint/MainActivity.java
+++ app/src/main/java/be/rapnhap/mywebprint/MainActivity.java
@@ -1,24 +1,43 @@
 package be.rapnhap.mywebprint;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintJob;
+import android.print.PrintManager;
+import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "Log Tag";
+
+    private WebView mWebView;
+
     Button buttonOtherActivity, buttonMap, buttonWebsite, buttonViewIntent, buttonIntentPro;
+    Button buttonWebPrint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // web page - open with browser
         //openWebPage("http://www.rapnhap.be");
 
+        // Intent and open other activities - example
         buttonOtherActivity = findViewById(R.id.buttonOtherActivity);
         buttonOtherActivity.setOnClickListener(view -> OnClick(view));
         buttonMap = findViewById(R.id.buttonMap);
@@ -29,8 +48,84 @@ public class MainActivity extends AppCompatActivity {
         buttonViewIntent.setOnClickListener(view -> OnClick(view));
         buttonIntentPro = findViewById(R.id.buttonIntentPro);
         buttonIntentPro.setOnClickListener(view -> OnClick(view));
-    }
 
+        // Web print example
+        buttonWebPrint = findViewById(R.id.buttonWebPrint);
+        buttonWebPrint.setOnClickListener(view -> OnClick(view));
+        }
+
+
+    // web print example
+    // ----------------------------------------
+    private void doWebViewPrint() {
+        // Create a WebView object specifically for printing
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient() {
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                Log.i(TAG, "page finished loading " + url);
+                createWebPrintJob(view);
+                mWebView = null;
+            }
+        });
+        // Generate an HTML document on the fly:
+        String htmlDocument = "<html><body><h1>Rap n Hap</h1>"+
+                "<table>" +
+
+                "<tr><td>1</td><td> A Bieslook</td><td style=\"text-align: right;\">2,00</td></tr>" +
+                "<tr><td>3</td><td> A Zalm</td><td style=\"text-align: right;\">9,00</td></tr>" +
+                "<tr><td>1</td><td> B Champignon</td><td style=\"text-align: right;\">3,00</td></tr>" +
+                "<tr><td>4</td><td> B Appel</td><td style=\"text-align: right;\">12,00</td></tr>" +
+                "<tr><td> </td><td> </td><td style=\"text-align: right;\">------</td></tr>" +
+                "<tr><td></td><td> <strong>TOTAAL</strong></td><td style=\"text-align: right;\">26,00</td></tr>" +
+
+                "</table></body></html>";
+        webView.loadDataWithBaseURL(null, htmlDocument, "text/HTML", "UTF-8", null);
+
+        // Keep a reference to WebView object until you pass the PrintDocumentAdapter
+        // to the PrintManager
+
+        mWebView = webView;
+    }
+        private void createWebPrintJob(WebView webView) {
+
+            // Get a PrintManager instance
+            PrintManager printManager = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                printManager = (PrintManager) this
+                        .getSystemService(Context.PRINT_SERVICE);
+            }
+
+            String jobName = getString(R.string.app_name) + " Document";
+
+            // Get a print adapter instance
+            PrintDocumentAdapter printAdapter = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                printAdapter = webView.createPrintDocumentAdapter(jobName);
+            }
+
+            // Create a print job with name and adapter instance
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                PrintAttributes.Builder builder = new PrintAttributes.Builder();
+                builder.setMediaSize(PrintAttributes.MediaSize.ISO_A4);
+                builder.setColorMode(PrintAttributes.COLOR_MODE_MONOCHROME);
+                PrintJob printJob = printManager.print(jobName, printAdapter,
+                        builder.build());
+            }
+
+            // Save the job object for later status checking
+            //printJobs.add(printJob);
+        }
+
+
+
+
+    // image print helper
     public void openWebPage(String url) {
         Uri webpage = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
@@ -64,6 +159,10 @@ public class MainActivity extends AppCompatActivity {
                 intent4.putExtra("KEY", "Key value to pass");
                 startActivity(intent4);
                 break;
+            case R.id.buttonWebPrint :
+                doWebViewPrint();
+                break;
+
         }
     }
 }
